6efbd4be30cb0eec775ebeb0b316b59583feaf46
Miarot <zinchenkodan@gmail.com>
2023-01-31 18:56:23 +0300

    make base for ik

--- __init__.py
+++ __init__.py
@@ -13,7 +13,8 @@ bl_info = {
 }
 
 # Load VPoser
-DIR_PATH = osp.dirname(osp.realpath(__file__))
+#DIR_PATH = osp.dirname(osp.realpath(__file__))
+DIR_PATH = "/home/miaro/itmo/anim/vposer_blender_addon" # test
 VP, _ = load_model(DIR_PATH + '/data/V02_05', model_code=VPoser,
                    remove_words_in_model_weights='vp_model.',
                    disable_grad=True)
@@ -91,6 +92,51 @@ class ObjectVPoserGeneratePose(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class ObjectVPoserIK(bpy.types.Operator):
+    """IK"""
+    bl_idname = "object.vposer_ik"
+    bl_label = "IK"
+    bl_options = {'REGISTER', 'UNDO'}
+
+
+    def execute(self, context):
+        if 'smplx_blender_addon' not in sys.modules:
+            self.report({"ERROR"}, "No smplx_blender_addon")
+            return {"CANCELLED"}
+
+        armature = context.object
+
+        if armature.type != 'ARMATURE':
+            self.report({"ERROR"}, "Current object is not armature")
+            return {"CANCELLED"}
+
+        smplx = sys.modules['smplx_blender_addon']
+        body_bones_names = \
+            smplx.SMPLX_JOINT_NAMES[1 : smplx.NUM_SMPLX_BODYJOINTS + 1]
+        root_name = smplx.SMPLX_JOINT_NAMES[0]
+
+        body_pose = []
+        root_orient = smplx.rodrigues_from_pose(armature, root_name)
+
+        for bone_name in body_bones_names:
+            body_pose.append(smplx.rodrigues_from_pose(armature, bone_name))
+
+        body_pose = torch.Tensor(body_pose).reshape((1,-1)).type(torch.float)
+        root_orient = torch.Tensor(root_orient).reshape((1,-1)).type(torch.float)
+        
+        body_pose_rec = body_pose
+        root_orient_rec = root_orient
+
+        for i, bone_name in enumerate(body_bones_names):
+            smplx.set_pose_from_rodrigues(armature, bone_name, 
+                                          body_pose_rec[0][3*i : 3*(i+1)])
+
+        smplx.set_pose_from_rodrigues(armature, root_name, root_orient_rec[0])
+
+        bpy.ops.object.smplx_set_poseshapes()
+
+        return {'FINISHED'}
+
 class SMPLX_PT_VPoser(bpy.types.Panel):
     bl_label = "VPoser"
     bl_category = "SMPL-X"
@@ -103,15 +149,18 @@ class SMPLX_PT_VPoser(bpy.types.Panel):
         
         col.operator("object.vposer_update_pose", text="Update")
         col.operator("object.vposer_generate_pose", text="Generate")
+        col.operator("object.vposer_ik", text="IK")
 
 def register():
     bpy.utils.register_class(ObjectVPoserUpdatePose)
     bpy.utils.register_class(ObjectVPoserGeneratePose)
+    bpy.utils.register_class(ObjectVPoserIK)
     bpy.utils.register_class(SMPLX_PT_VPoser)
 
 def unregister():
     bpy.utils.unregister_class(ObjectVPoserPoseUpdate)
     bpy.utils.unregister_class(ObjectVPoserGeneratePose)
+    bpy.utils.unregister_class(ObjectVPoserIK)
     bpy.utils.unregister_class(SMPLX_PT_VPoser)
 
 if __name__ == "__main__":
